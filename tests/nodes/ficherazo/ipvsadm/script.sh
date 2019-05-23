b7aff8bf377da876734acc87e07f6d2868e66d64
Pablo-CC <p.celaya.crespo@gmail.com>
2019-05-23 13:04:12 +0200

    Add tests folder

---
+++ tests/nodes/ficherazo/ipvsadm/script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+fecha="11/14/2018"
+hora_inicio="13:25"
+hora_final="13:39"
+title=ipvsadm_16GB
+metrics=('bytes_in' 'bytes_out' 'pkts_in' 'pkts_out' 'cpu_idle' 'cpu_system' 'cpu_user' 'cpu_wio' 'mem_buffers' 'mem_cached' 'mem_free')
+regexp="wn019|wn020|wn021|wn022"
+nodes="wn019.macc.unican.es|meteo,wn020.macc.unican.es|meteo,wn021.macc.unican.es|meteo,wn022.macc.unican.es|meteo"
+
+for metric in "${metrics[@]}"
+do
+  wget "meteo.unican.es/ganglia/graph.php?r=hour&z=xlarge&title=$metric&mreg[]=^$metric$&hreg[]=$regexp&aggregate=1&hl=$nodes&cs=$fecha+$hora_inicio+&ce=$fecha+$hora_final&csv=1" -O $metric.csv
+  wget "meteo.unican.es/ganglia/graph.php?r=hour&z=xlarge&title=$metric&mreg[]=^$metric$&hreg[]=$regexp&aggregate=1&hl=$nodes&cs=$fecha+$hora_inicio+&ce=$fecha+$hora_final" -O $metric.png
+done
